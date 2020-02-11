1857f3caf190e1cd25b018ecb3186661fe8fab7e
Jinwoo Kim <kidjinwoo@me.com>
2020-02-10 22:43:48 -0800

    Update exercise.c

--- Chapter_3/exercise.c
+++ Chapter_3/exercise.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
+    printf("temp conversion\n");
     float a;
     printf("Enter stating temp: ");
     //scanf("%6.2f", &a);
@@ -14,4 +15,20 @@ int main(void){
         
         a += 5;
     }
+
+    //
+
+    printf("lb(pounds) to kg\n");
+    float b;
+    printf("Enter lb: ");
+    scanf("%f", &b);
+    printf("Result: %fkg\n", b / 45359237E-7);
+
+    //
+
+    printf("miles to kilometers\n");
+    float c;
+    printf("Enter miles: ");
+    scanf("%f", &c);
+    printf("Result: %fkm\n", c / 62137E-5);
 }
